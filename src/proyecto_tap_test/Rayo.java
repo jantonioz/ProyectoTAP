e29d1c49a69d90c8c2018a79c946fc981fbd2c44
Marcos Saucedo <marcos.saucedo@metricamovil.com>
2019-05-22 19:13:45 -0500

    colores

--- src/proyecto_tap_test/Rayo.java
+++ src/proyecto_tap_test/Rayo.java
@@ -18,16 +18,47 @@ public class Rayo {
     int r = 30;
     PointF dir;
     double angulo;
+    int R, G, B, incR, incG, incB;
 
     public Rayo(PointF start, double angle) {
         this.inicio = start;
         this.dir = calcDir();
         this.angulo = angle;
+        R = 255;
+        G = B = incR = incG = incB = 0;
     }
 
     public void Actualizar(PointF p) {
         this.inicio = new PointF(p.x, p.y);
         dir = calcDir();
+        R += incR;
+        G += incG;
+        B += incB;
+
+        if (R == 255 && G == 0 && B == 0) {
+            incB = 0;
+            incG = 5;
+        }
+        if (R == 255 && G == 255 && B == 0) {
+            incG = 0;
+            incR = -5;
+        }
+        if (R == 0 && G == 255 && B == 0) {
+            incR = 0;
+            incB = 5;
+        }
+        if (R == 0 && G == 255 && B == 255) {
+            incB = 0;
+            incG = -5;
+        }
+        if (R == 0 && G == 0 && B == 255) {
+            incG = 0;
+            incR = 5;
+        }
+        if (R == 255 && G == 0 && B == 255) {
+            incR = 0;
+            incB = -5;
+        }
     }
 
     public void setFin(PointF pt) {
@@ -92,11 +123,12 @@ public class Rayo {
 
         for (int i = 0; i <= steps; i++) {
             double colorAux = color >= 0 ? color : 0;
-            g.setColor(new Color((int) colorAux, (int) colorAux, (int) colorAux));
+//            g.setColor(new Color((int) colorAux, (int) colorAux, (int) colorAux));
+            g.setColor(new Color((int)(R * colorAux / 255), (int)(G * colorAux / 255), (int)(B * colorAux / 255)));
             g.drawLine((int) x, (int) y, (int) x, (int) y);
             x += incX;
             y += incY;
-            color -= 0.5 * hyp / steps;
+            color -= 0.6 * hyp / steps;
         }
     }
 }
